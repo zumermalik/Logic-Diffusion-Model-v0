47b2d0277b4d75dcbb55a72d087b7859dfec3763
zumermalik <zumermalik48@gmail.com>
2026-01-02 23:49:01 +0500

    finalizing with init.py

--- logic_diffusion/__init__.py
+++ logic_diffusion/__init__.py
@@ -1,1 +1,12 @@
+from .config import TrainingConfig
+from .logic import DifferentiableLogic, LogicConstraint
+from .modeling import SimpleUNet
+from .pipeline import LogicGuidedPipeline
 
+__all__ = [
+    "TrainingConfig",
+    "DifferentiableLogic",
+    "LogicConstraint",
+    "SimpleUNet",
+    "LogicGuidedPipeline"
+]
